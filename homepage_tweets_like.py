eccdf7a41445ec08f497d2e47f967877207e564c
Shubham Singh <64825259+Shubham722-227@users.noreply.github.com>
2022-10-02 15:29:49 +0530

    edited comment

--- homepage_tweets_like.py
+++ homepage_tweets_like.py
@@ -1,7 +1,7 @@
 import os
 from twitter_bot_class import Twitter_Bot
 """
-Automatically likes the tweets in your feed
+Automatically likes the tweets in your feed.
 """
 if __name__ == "__main__":
     EMAIL = input("Provide your Phone, email or username: ")
